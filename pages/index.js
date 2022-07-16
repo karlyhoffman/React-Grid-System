68dc1ce2e387390fbbfd66feb4d2e6d6cf5e6d65
Karly Hoffman <karlyhoffman@gmail.com>
2022-07-16 11:47:29 -0500

    grid system files

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,121 @@
-import {} from 'components';
+import { Row, Column } from 'components';
 
 export default function Home() {
   return (
     <main>
-      <h1>React Grid System</h1>
+      <Row>
+        <Column>
+          <h1 style={{ textAlign: 'center' }}>React Grid System</h1>
+        </Column>
+
+        <Column md={6} xl={4}>
+          <h2>Column</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Magna sit amet purus gravida quis blandit.
+            Non pulvinar neque laoreet suspendisse. Dictum non consectetur a erat nam. Sagittis nisl
+            rhoncus mattis rhoncus urna neque viverra. Et netus et malesuada fames ac turpis egestas
+            maecenas. Nunc mi ipsum faucibus vitae aliquet nec. Cursus turpis massa tincidunt dui
+            ut. Risus pretium quam vulputate dignissim suspendisse in est. Egestas pretium aenean
+            pharetra magna ac placerat. Faucibus et molestie ac feugiat sed lectus. Elementum
+            curabitur vitae nunc sed velit dignissim sodales ut eu.
+          </p>
+          <p>
+            Integer malesuada nunc vel risus commodo viverra maecenas accumsan. Urna neque viverra
+            justo nec ultrices. Quis vel eros donec ac odio tempor. Neque gravida in fermentum et
+            sollicitudin ac orci phasellus. Sed felis eget velit aliquet sagittis id consectetur
+            purus. Ut lectus arcu bibendum at varius vel pharetra. Auctor urna nunc id cursus metus.
+            Nunc vel risus commodo viverra maecenas accumsan. Consectetur adipiscing elit ut aliquam
+            purus. Quis auctor elit sed vulputate. Egestas tellus rutrum tellus pellentesque eu
+            tincidunt tortor aliquam nulla. Sit amet porttitor eget dolor morbi non. Volutpat odio
+            facilisis mauris sit amet massa vitae. Tellus in metus vulputate eu scelerisque felis
+            imperdiet proin. Tempor id eu nisl nunc mi ipsum faucibus. Sed cras ornare arcu dui
+            vivamus. Vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Dolor
+            magna eget est lorem ipsum dolor sit. Id diam vel quam elementum pulvinar etiam non.
+            Sagittis vitae et leo duis ut diam quam nulla porttitor.
+          </p>
+        </Column>
+
+        <Column md={6} xl={4}>
+          <h2>Column</h2>
+          <p>
+            Fringilla urna porttitor rhoncus dolor purus non. Pellentesque adipiscing commodo elit
+            at imperdiet dui accumsan sit amet. Vulputate odio ut enim blandit volutpat maecenas
+            volutpat blandit aliquam. Egestas fringilla phasellus faucibus scelerisque eleifend
+            donec pretium vulputate sapien. Porttitor eget dolor morbi non. Nibh tellus molestie
+            nunc non blandit massa enim. Feugiat nisl pretium fusce id velit ut. Viverra vitae
+            congue eu consequat ac felis donec. Congue quisque egestas diam in. Aliquet nibh
+            praesent tristique magna sit. Aenean vel elit scelerisque mauris pellentesque pulvinar.
+            Porttitor leo a diam sollicitudin tempor id eu nisl. Fames ac turpis egestas maecenas
+            pharetra convallis posuere morbi. Curabitur gravida arcu ac tortor dignissim convallis
+            aenean et. Amet aliquam id diam maecenas. Eu non diam phasellus vestibulum lorem sed
+            risus ultricies tristique. Lobortis scelerisque fermentum dui faucibus in ornare quam
+            viverra. Urna et pharetra pharetra massa massa ultricies mi quis hendrerit. Imperdiet
+            sed euismod nisi porta lorem mollis aliquam ut porttitor. Amet facilisis magna etiam
+            tempor orci eu lobortis.
+          </p>
+          <p>
+            Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Ac odio tempor orci dapibus
+            ultrices. Amet facilisis magna etiam tempor orci. Tellus rutrum tellus pellentesque eu
+            tincidunt tortor aliquam nulla facilisi. Egestas pretium aenean pharetra magna. Eget
+            gravida cum sociis natoque penatibus et. Donec ac odio tempor orci dapibus ultrices.
+          </p>
+        </Column>
+
+        <Column md={6} xl={4}>
+          <h2>Column</h2>
+          <p>
+            Ut sem nulla pharetra diam sit. Et tortor at risus viverra adipiscing at in tellus
+            integer. In dictum non consectetur a erat nam at. Non enim praesent elementum facilisis
+            leo vel fringilla est. Dolor sit amet consectetur adipiscing elit duis. Metus aliquam
+            eleifend mi in nulla posuere sollicitudin aliquam ultrices. Morbi enim nunc faucibus a
+            pellentesque sit. Id porta nibh venenatis cras. Eu scelerisque felis imperdiet proin
+            fermentum leo vel orci. Facilisi nullam vehicula ipsum a arcu.
+          </p>
+          <p>
+            Integer malesuada nunc vel risus commodo viverra maecenas accumsan. Urna neque viverra
+            justo nec ultrices. Quis vel eros donec ac odio tempor. Neque gravida in fermentum et
+            sollicitudin ac orci phasellus. Sed felis eget velit aliquet sagittis id consectetur
+            purus. Ut lectus arcu bibendum at varius vel pharetra. Auctor urna nunc id cursus metus.
+            Nunc vel risus commodo viverra maecenas accumsan. Consectetur adipiscing elit ut aliquam
+            purus. Quis auctor elit sed vulputate. Egestas tellus rutrum tellus pellentesque eu
+            tincidunt tortor aliquam nulla. Sit amet porttitor eget dolor morbi non. Volutpat odio
+            facilisis mauris sit amet massa vitae. Tellus in metus vulputate eu scelerisque felis
+            imperdiet proin. Tempor id eu nisl nunc mi ipsum faucibus.
+          </p>
+          <p>
+            Dolor magna eget est lorem ipsum dolor sit. Id diam vel quam elementum pulvinar etiam
+            non. Sagittis vitae et leo duis ut diam quam nulla porttitor. Amet facilisis magna etiam
+            tempor orci. Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla
+            facilisi. Egestas pretium aenean pharetra magna.
+          </p>
+        </Column>
+
+        <Column md={6} mdOffset={3}>
+          <h2>Offset Column Example</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Magna sit amet purus gravida quis blandit.
+            Non pulvinar neque laoreet suspendisse. Dictum non consectetur a erat nam. Sagittis nisl
+            rhoncus mattis rhoncus urna neque viverra. Et netus et malesuada fames ac turpis egestas
+            maecenas. Nunc mi ipsum faucibus vitae aliquet nec. Cursus turpis massa tincidunt dui
+            ut. Risus pretium quam vulputate dignissim suspendisse in est. Egestas pretium aenean
+            pharetra magna ac placerat. Faucibus et molestie ac feugiat sed lectus. Elementum
+            curabitur vitae nunc sed velit dignissim sodales ut eu.
+          </p>
+          <p>
+            Integer malesuada nunc vel risus commodo viverra maecenas accumsan. Urna neque viverra
+            justo nec ultrices. Quis vel eros donec ac odio tempor. Neque gravida in fermentum et
+            sollicitudin ac orci phasellus. Sed felis eget velit aliquet sagittis id consectetur
+            purus. Ut lectus arcu bibendum at varius vel pharetra. Auctor urna nunc id cursus metus.
+            Nunc vel risus commodo viverra maecenas accumsan. Consectetur adipiscing elit ut aliquam
+            purus. Quis auctor elit sed vulputate. Egestas tellus rutrum tellus pellentesque eu
+            tincidunt tortor aliquam nulla. Sit amet porttitor eget dolor morbi non. Volutpat odio
+            facilisis mauris sit amet massa vitae. Tellus in metus vulputate eu scelerisque felis
+            imperdiet proin. Tempor id eu nisl nunc mi ipsum faucibus.
+          </p>
+        </Column>
+      </Row>
     </main>
   );
 }
